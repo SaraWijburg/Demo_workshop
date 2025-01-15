5ab7ab4db843dfabdf1546474ad82705a70ad37d
Sara Wijburg <wijburgs@rivm-lsfsv-l09p.rivm.ssc-campus.nl>
2025-01-15 09:31:43 +0100

    added packages and scripts

--- R/Masterscript.R
+++ R/Masterscript.R
@@ -4,16 +4,19 @@
 #----------------------------------------------------------------------------------------------------------#
 ############################################################################################################
 
-# remotes::install_github("r-spatial/mapview")
+
 # Welke packages worden gebruikt in dit script?
 gebruikte_packages <- c(
-  "raster", "terra", "ncdf4", "stars", "RPostgreSQL", "mapview", 
-  "data.table", "tidytable", "tidyverse", "dplyr", "readxl", "writexl", "httr", "jsonlite", 
-  "cowplot", "RColorBrewer", "broom", "dbplyr", "moments", "XML", "methods", "foreign", 
-  "lubridate", "janitor", "ggpubr", "corrplot", "Hmisc", "PerformanceAnalytics", "classInt", "sf", "stringi", "data.table",
-  "boot", "viridis","rtrim", "ggspatial")
-
-# package:exactextractr
+  "raster", "terra", "ncdf4", "stars", "RPostgreSQL", "mapview",
+  "data.table", "tidytable", "tidyverse", "dplyr", "readxl",
+  "writexl", "httr", "jsonlite",
+  "cowplot", "RColorBrewer", "broom", "dbplyr", "moments",
+  "XML", "methods", "foreign",
+  "lubridate", "janitor", "ggpubr", "corrplot", "Hmisc",
+  "PerformanceAnalytics", "docstring",
+  "classInt", "sf", "stringi", "data.table",
+  "boot", "viridis", "rtrim", "ggspatial", "lintr", "styler" # lintr package gaat door je code heen om te kijken of het volgens de style correct is, styler maakt het clean
+)
 
 # Welke packages zijn al geinstallerd?
 geinstalleerde_packages <- rownames(installed.packages())
@@ -22,14 +25,32 @@ geinstalleerde_packages <- rownames(installed.packages())
 installeren_packages <- gebruikte_packages[!gebruikte_packages %in% geinstalleerde_packages]
 if (length(installeren_packages) > 0) install.packages(pkgs = installeren_packages)
 
-# Laad packages als ze nog niet attached zijn
-# zonder messages!
+# Laad packages
 if (!all(gebruikte_packages %in% .packages())) {
   suppressMessages(invisible(lapply(
     X              = gebruikte_packages,
     FUN            = library,
-    character.only = TRUE)))
+    character.only = TRUE
+  )))
 }
 
-options(scipen=999) # Remove scientific notation 
+options(scipen = 999) # Remove scientific notation
 select <- dplyr::select
+
+# lintr::lint("R/Masterscript.R")
+# styler:::style_active_file()
+
+#----------------------------------------------------------------------------------------------------------#
+# Functions
+#----------------------------------------------------------------------------------------------------------#
+source(file = "R/Functions.R")
+
+#----------------------------------------------------------------------------------------------------------#
+# Analyses
+#----------------------------------------------------------------------------------------------------------#
+source(file = "R/Calculate_distance_point_to_line.R")
+
+
+
+
+
