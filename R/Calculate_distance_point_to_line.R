0b63be6292b552dcfbc19ad76d5a937773e02b7b
Sara Wijburg <wijburgs@rivm-lsfsv-l13p.rivm.ssc-campus.nl>
2025-01-15 12:01:39 +0100

    toevoegen dat er wordt gewerkt met nep data

--- R/Calculate_distance_point_to_line.R
+++ R/Calculate_distance_point_to_line.R
@@ -3,27 +3,35 @@
 # Title: Calculate distances point to line
 #----------------------------------------------------------------------------------------------------------#
 ############################################################################################################
-
+simulate <- FALSE
 #----------------------------------------------------------------------------------------------------------#
 #-------------- TBEV data 
 #----------------------------------------------------------------------------------------------------------#
-coordinates_tbev_org <- read_xlsx("data/raw/Terschelling_friends.xlsx")
-data_tbev_coord_df   <- coordinates_tbev_org
 
-data_tbev_coord_sf <- data_tbev_coord_df  %>% 
-  select(tbevID = label, country, microfocus, collection_date, latitude, longitude) %>% 
-  mutate(VID = str_c("V", row_number())) %>% 
-  st_as_sf(coords = c("longitude", "latitude"), crs = "EPSG:4326", remove = TRUE) %>%
-  # st_as_sf(coords = c("longitude", "latitude"), crs = "EPSG:3035", remove = TRUE) #%>% 
-  st_transform(crs = st_crs("+proj=longlat +datum=WGS84"))
+if(simulate){
+  coordinates_tbev_org <- read_xlsx("data/raw/Terschelling_friends.xlsx")
+  data_tbev_coord_df   <- coordinates_tbev_org
+  
+  data_tbev_coord_sf <- data_tbev_coord_df  %>% 
+    select(tbevID = label, country, microfocus, collection_date, latitude, longitude) %>% 
+    mutate(VID = str_c("V", row_number())) %>% 
+    st_as_sf(coords = c("longitude", "latitude"), crs = "EPSG:4326", remove = TRUE) %>%
+    # st_as_sf(coords = c("longitude", "latitude"), crs = "EPSG:3035", remove = TRUE) #%>% 
+    st_transform(crs = st_crs("+proj=longlat +datum=WGS84"))
   # st_transform(crs = 3035)
+}
+
 
 #----------------------------------------------------------------------------------------------------------#
 #-------------- Bird data (black bird)
 #----------------------------------------------------------------------------------------------------------#
-data_bird_org <- readRDS("data/raw/data_Turdus_merula_20241213_1210.RDS")
-data_bird     <- data_bird_org %>% 
-  select(Ring, Species = current_name, Latitude, Longitude, Date, Distance, Country)
+
+
+if(simulate){
+  data_bird_org <- readRDS("data/raw/data_Turdus_merula_20241213_1210.RDS")
+  data_bird     <- data_bird_org %>% 
+    select(Ring, Species = current_name, Latitude, Longitude, Date, Distance, Country)
+}
 
 #-------------- Determine trajectories 
 bird_lines_sf <- function_determine_trajectory(data = data_bird)
@@ -32,15 +40,9 @@ bird_lines_sf <- function_determine_trajectory(data = data_bird)
 tmp_distance_trans <- function_distance_point_lines(bird_lines_sf)
 
 #-------------- Determine combinations 
-
-
-
-
- 
-
 tmp_distance_trans
   full_join(tmp_distance_trans, by = c("trajectoryID", "Ring"), relationship = "many-to-many") %>% 
-  filter(tbevID.x == "DZIF23_597") %>% 
+  # filter(tbevID.x == "DZIF23_597") %>% 
   filter(tbevID.x != tbevID.y) %>% 
   filter(distance.y <= 50)
 
@@ -54,10 +56,10 @@ tmp_distance_trans
 # determine which lines in which ranges
 # determmine which lines match with lines and how many lines 
 
-ggplot() + 
-  geom_sf(data_tbev_coord_sf %>% 
-            filter(tbevID == "DZIF23_597"), mapping = aes(geometry = geometry, fill = tbevID)) + 
-  geom_sf(bird_lines_sf %>% filter(trajectoryID == 12122), mapping = aes(geometry = geometry))
+# ggplot() + 
+#   geom_sf(data_tbev_coord_sf %>% 
+#             filter(tbevID == "DZIF23_597"), mapping = aes(geometry = geometry, fill = tbevID)) + 
+#   geom_sf(bird_lines_sf %>% filter(trajectoryID == 12122), mapping = aes(geometry = geometry))
 
 
 
